9bd552a4dd253f129e4fd77250338a66f608e4fb
Victory Ndu <victor5ndu@gmail.com>
2023-07-25 20:28:39 +0100

    Add endpoint to project

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>hello {{ data }}</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+
+  data(){
+    return{
+      data: []
+    }
+  },
+
+  methods:{
+    getDogs(){
+      this.$http.get(`?limit=10?api_key=${process.env.VUE_APP_DOG_API_KEY}`)
+      .then(response =>{
+        console.log(response)
+        this.data = response
+      }).catch(error =>{
+        console.log(error)
+      })
+    }
+  },
+
+  mounted(){
+    this.getDogs()
   }
 }
 </script>
