43abdb991563303e43f06438bff5d5d0e6e341ee
Victory Ndu <victor5ndu@gmail.com>
2023-07-26 13:42:56 +0100

    add tailwind

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from './plugins/axios'
+import './assets/tailwind.css'
 
 Vue.config.productionTip = false
 Vue.prototype.$http = axios;
