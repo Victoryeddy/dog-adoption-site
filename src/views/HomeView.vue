e0c22f28030d2ede2d70f0f38b2ff85bbe571c8e
Victory Ndu <victor5ndu@gmail.com>
2023-07-27 22:18:12 +0100

    configure tailwind correctly

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,43 @@
 <template>
-  <div class="home">
-    <p v-for="single in data" :key="single.id">
-     <img :src="single.image.url" width="500" height="400" />
-    </p>
+  <div class="home container mx-auto">
+    <div class="grid grid-cols-3 gap-4">
+      
+      <div v-for="single in data" :key="single.id">
+       <img :src="single.image.url" width="400" height="400" />
+      </div>
+   </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 export default {
-  name: 'HomeView',
+  name: "HomeView",
 
-  data(){
-    return{
+  data() {
+    return {
       data: [],
       currentPage: 0,
     }
   },
 
-  methods:{
-    getDogs(){
+  methods: {
+    getDogs() {
       // this.$http.get(`?limit=20?page=0-2`)
-      this.$http.get(`?limit=15&page=${this.currentPage}`)
-      .then(response =>{
-        console.log(response.data)
-        this.data = response.data
-      }).catch(error =>{
-        console.log(error)
-      })
-    }
+      this.$http
+        .get(`?limit=15&page=${this.currentPage}`)
+        .then((response) => {
+          console.log(response.data)
+          this.data = response.data
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    },
   },
 
-  mounted(){
+  mounted() {
     this.getDogs()
-  }
+  },
 }
 </script>
---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  content: [
+    './src/**/*.vue',
+    './src/**/*.js',
+    './src/**/*.css',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
