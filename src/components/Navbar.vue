596423c2a22b2683570b131c2f6c79a6758bc7de
Victory Ndu <victor5ndu@gmail.com>
2023-10-08 10:23:20 +0100

    Add navbar to small screen size

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,31 @@
 <template>
   <div class="shadow-sm">
-    <div class="flex flex-row justify-between container mx-auto">
+    <div class="flex flex-row justify-between container mx-auto px-2">
       <!-- <div> -->
-      <img
-        src="../assets/Dog_logo-removebg-preview.png"
-        alt=""
-        width="100"
-        height="100"
-      />
-      <!-- </div> -->
+
+      <div>
+        <img
+          src="../assets/Dog_logo-removebg-preview.png"
+          alt=""
+          width="100"
+          height="100"
+        />
+      </div>
+
+      <button class="mt-4 p-3 lg:hidden" @click="toggleNavBar">
+        <img src="../assets/align-right.svg" alt=""  />
+      </button>
+
       <div class="hidden lg:flex lg:items-center">
-        <router-link class="px-3 font-medium" to="/">Home</router-link>
-        <!-- <router-link class="px-3 font-medium" to="/">About</router-link> -->
-        <router-link class="px-3 font-medium" to="/gallery"
+        <router-link class="px-3 font-medium hover:underline" to="/">Home</router-link>
+        <router-link class="px-3 font-medium hover:underline" to="/gallery"
           >Gallery</router-link
         >
-         <router-link v-if="$auth.isAuthenticated" to="/profile"
-         class="btn-outline ms-3" >Profile</router-link
+        <router-link
+          v-if="$auth.isAuthenticated"
+          to="/profile"
+          class="btn-outline ms-3"
+          >Profile</router-link
         >
         <div v-if="!$auth.loading" class="ms-4">
           <!-- show login when not authenticated -->
@@ -28,11 +37,38 @@
             Log out
           </button>
         </div>
-
-       
-        <!-- <router-link class="px-3 font-medium" to="/">Blog</router-link> -->
-        <!-- <router-link class="px-3 font-medium" to="/">Contact</router-link> -->
       </div>
+    </div>
+
+    <div class="lg:hidden" v-show="showSmallScreenNavBar">
+      <ul class="text-right">
+        <li class="my-5 border border-t-0 border-b-2 border-s-0 border-e-0 border-gray-400">
+          <router-link class="px-3 font-medium" to="/">Home</router-link>
+        </li>
+        <li class="my-5  border border-b-2 border-t-0 border-s-0 border-e-0 border-gray-400">
+          <router-link class="px-3 font-medium" to="/gallery"
+            >Gallery</router-link
+          >
+        </li>
+        <li class="my-5">
+          <router-link
+            v-if="$auth.isAuthenticated"
+            to="/profile"
+            class="btn-outline ms-3"
+            >Profile</router-link
+          >
+        </li>
+        <li class="my-5">
+          <div v-if="!$auth.loading" class="ms-4">
+            <button v-if="!$auth.isAuthenticated" @click="login" class="btn">
+              Log in
+            </button>
+            <button v-if="$auth.isAuthenticated" @click="logout" class="btn">
+              Log out
+            </button>
+          </div>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
@@ -40,7 +76,14 @@
 export default {
   name: "Navbar",
   data() {
-    return {};
+    return {
+      showSmallScreenNavBar: false,
+    };
+  },
+
+
+  watch: {
+    
   },
 
   methods: {
@@ -56,6 +99,10 @@ export default {
         },
       });
     },
+
+    toggleNavBar(){
+      this.showSmallScreenNavBar = !this.showSmallScreenNavBar;
+    }
   },
 };
 </script>
